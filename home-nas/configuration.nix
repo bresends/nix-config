bbb0f8c2d23896ff719388fc2982b4de790de020
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-11-23 16:23:25 -0300

    Initial commit: Add NixOS configuration files

---
+++ home-nas/configuration.nix
@@ -0,0 +1,196 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+ # Enable networking
+  networking = {
+      hostName = "nixos";
+
+      # Configure static IP on your interface
+      interfaces.enp0s31f6 = {
+          useDHCP = false;
+          ipv4.addresses = [{
+              address = "192.168.0.10";
+              prefixLength = 24;
+          }];
+      };
+
+      # Set default gateway
+      defaultGateway = "192.168.0.1";
+
+      # Cloudflare DNS servers
+      nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  };
+
+   systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+    AllowSuspendThenHibernate=no
+    AllowHybridSleep=no
+  '';
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "pt_BR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.bruno = {
+    isNormalUser = true;
+    description = "bruno";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "bruno";
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+	  vim
+	  mpv
+	  htop
+          rsync
+  ];
+
+  fileSystems."/mnt/tank" = {
+	  device = "/dev/disk/by-uuid/afa4df74-f0ef-479f-92e3-1f9314e153d5";
+	  fsType = "ext4";
+	  options = [ "defaults" ];
+  };
+
+  services.samba = {
+	  enable = true;
+	  openFirewall = true;
+	  settings = {
+		  global = {
+			  "workgroup" = "WORKGROUP";
+			  "server string" = "smbnix";
+			  "netbios name" = "smbnix";
+			  "security" = "user";
+			  "guest account" = "nobody";
+			  "map to guest" = "bad user";
+		  };
+		  "media" = {
+			  "path" = "/mnt/tank/media";
+			  "browseable" = "yes";
+			  "read only" = "no";
+			  "guest ok" = "yes";
+			  "create mode" = "0777";
+			  "directory mode" = "0777";
+		  };
+	  };
+  };
+
+  services.samba-wsdd = {
+	  enable = true;
+	  openFirewall = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  virtualisation.docker = {
+	  enable = true;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
