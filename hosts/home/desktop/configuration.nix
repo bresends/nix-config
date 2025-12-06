75b473caed7c0bf36bf8df61f8fa2b7a0f01999a
= <bruno.resende@gmx.com>
2025-12-06 14:20:09 -0300

    fix(shamba): change desktop config to have proper file permissions

--- hosts/home/desktop/configuration.nix
+++ hosts/home/desktop/configuration.nix
@@ -36,7 +36,7 @@
   fileSystems."/mnt/nas" = {
     device = "//192.168.0.10/media";
     fsType = "cifs";
-    options = [ "guest" "nofail" ];
+    options = [ "guest" "nofail" "uid=1000" "gid=100" ];
   };
 
   # Define user account
