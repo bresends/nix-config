e3248e323d0fc6b216fb610d6d35d62af745384a
= <bruno.resende@gmx.com>
2025-12-07 16:36:01 -0300

    feat: add nix-sops for secrets

--- modules/server/nginx-test.nix
+++ modules/server/nginx-test.nix
@@ -1,11 +1,38 @@
-{ config, pkgs, ... }:
+{ config, pkgs, sops-nix, ... }:
 
 {
+  imports = [
+    sops-nix.nixosModules.sops
+  ];
+
+  # Configure sops to read secrets
+  sops.defaultSopsFile = ../../../secrets/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/home/bruno/.config/sops/age/keys.txt";
+
+  # Template to create the credentials file in the format ACME expects
+  sops.templates."cloudflare-credentials".content = ''
+    CLOUDFLARE_DNS_API_TOKEN=${config.sops.placeholder."cloudflare/api_token"}
+  '';
+  sops.templates."cloudflare-credentials".owner = "acme";
+
+  # Configure ACME with Cloudflare DNS
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "bruno@cbmgo.org";
+      dnsProvider = "cloudflare";
+      credentialsFile = config.sops.templates."cloudflare-credentials".path;
+    };
+  };
+
   services.nginx = {
     enable = true;
     virtualHosts."test.home.cbmgo.org" = {
+      forceSSL = true;
+      enableACME = true;
       locations."/" = {
-        return = "200 'Hello from NixOS nginx!'";
+        return = "200 'Hello from NixOS nginx with SSL!'";
         extraConfig = ''
           add_header Content-Type text/plain;
         '';
