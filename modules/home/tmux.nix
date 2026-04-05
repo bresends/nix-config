f1c5d3dfd5ffd6b0b4a9e7f461ec2b096c684dd0
= <bruno.resende@gmx.com>
2026-04-05 09:59:22 -0300

    feat(tmux): add tmux plugins

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -1,7 +1,31 @@
 { config, pkgs, ... }:
 
+let
+  tmux-uptime = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-uptime";
+    version = "unstable";
+    src = pkgs.fetchFromGitHub {
+      owner = "robhurring";
+      repo = "tmux-uptime";
+      rev = "master";
+      sha256 = "sha256-hKVx9MIpuoa0bnv6CsW2pD/kOmggbdBipyWj3pKMDR4=";
+    };
+  };
+in
 {
   programs.tmux = {
     enable = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      cpu
+      resurrect
+      continuum
+      vim-tmux-navigator
+      tmux-uptime
+    ];
+    extraConfig = ''
+      set -g @resurrect-capture-pane-contents 'on'
+      set -g @continuum-restore 'on'
+    '';
   };
 }
