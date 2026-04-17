867d284eda453c5a520135a1097e79d960674845
Bruno Resende <bruno.resendesantos45@gmail.com>
2026-04-17 16:08:49 -0300

    fix(8bbm-sgp-ws02): remove filesystem definitions conflicting with disko
    The hardware-configuration.nix fileSystems and swapDevices
    entries conflict with disk-config.nix managed by disko.

--- hosts/8bbm-sgp-ws02/hardware-configuration.nix
+++ hosts/8bbm-sgp-ws02/hardware-configuration.nix
@@ -10,27 +10,9 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1195a358-225e-4166-aaf6-68d1c907cb92";
-      fsType = "ext4";
-    };
+  # Filesystem and swap managed by disko (disk-config.nix)
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5764-EC07";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/6c7af131-6a4f-4ee6-a928-b6f99040cf54"; }
-    ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.en0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
