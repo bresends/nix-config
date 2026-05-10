046e617bcb4af4bfb0bc6be10c54d58761e3c11f
= <bruno.resende@gmx.com>
2026-05-10 16:35:32 -0300

    fix(fcitx5): enable waylandFrontend to suppress GTK/QT IM module warning
    With LC_CTYPE=pt_BR.UTF-8 in place for cedilla, test waylandFrontend
    to avoid the GTK_IM_MODULE/QT_IM_MODULE warning on KDE Wayland.

--- modules/nixos/fcitx5.nix
+++ modules/nixos/fcitx5.nix
@@ -4,6 +4,9 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [ qt6Packages.fcitx5-configtool fcitx5-gtk ];
+    fcitx5 = {
+      addons = with pkgs; [ qt6Packages.fcitx5-configtool fcitx5-gtk ];
+      waylandFrontend = true;
+    };
   };
 }
