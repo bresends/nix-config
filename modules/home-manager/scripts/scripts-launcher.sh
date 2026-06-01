cbe49f51309e5d9487e899937df1160782ddeba4
= <bruno.resende@gmx.com>
2026-06-01 00:09:31 -0300

    feat(nix): check for changes in flake.lock before committing

--- modules/home-manager/scripts/scripts-launcher.sh
+++ modules/home-manager/scripts/scripts-launcher.sh
@@ -19,11 +19,15 @@ case "$choice" in
             notify-send "System Update" "Starting flake update in the background..."
             cd "$flake_dir"
             if nix flake update; then
-                git add flake.lock
-                if git commit -m "chore(system): update flake"; then
-                    notify-send "System Update" "Flake update complete and committed!"
+                if git diff --quiet flake.lock; then
+                    notify-send "System Update" "Flake inputs are already up to date!"
                 else
-                    notify-send "System Update" "Flake updated, but git commit failed." --urgency=critical
+                    git add flake.lock
+                    if git commit -m "chore(system): update flake"; then
+                        notify-send "System Update" "Flake update complete and committed!"
+                    else
+                        notify-send "System Update" "Flake updated, but git commit failed." --urgency=critical
+                    fi
                 fi
             else
                 notify-send "System Update" "nix flake update failed!" --urgency=critical
