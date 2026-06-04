8772604bfd9b14f80fa0f6da815fdd4081b35a99
= <bruno.resende@gmx.com>
2026-06-04 15:00:33 -0300

    feat(bookmarks): add CBMGO websites to bookmarks list

--- modules/home-manager/scripts/bookmarks.sh
+++ modules/home-manager/scripts/bookmarks.sh
@@ -12,6 +12,10 @@ Radarr | http://radarr.local.cbmgo.org
 Lidarr | http://lidarr.local.cbmgo.org
 Bazarr | http://bazarr.local.cbmgo.org
 qBittorrent | http://qbittorrent.local.cbmgo.org
+CBMGO Contatos e Endereços | https://www.bombeiros.go.gov.br/acesso-a-informacao/contatos-enderecos-horarios-cargos-e-ocupantes
+CBMGO Normas Administrativas | https://www.bombeiros.go.gov.br/legislacao/normas-administrativas-2.html
+CBMGO Normas Operacionais | https://www.bombeiros.go.gov.br/legislacao/normas-operacionais-administrativas/normas-operacionais-2.html
+CBMGO Manuais | https://www.bombeiros.go.gov.br/legislacao/manuais-de-bombeiros/manuais-de-bombeiros.html
 EOF
 )
 
