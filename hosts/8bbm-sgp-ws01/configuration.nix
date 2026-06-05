93030f1f5754b7f3639412bbc0f4e0129dd186cd
Bruno Resende <bruno.resendesantos45@gmail.com>
2026-06-05 08:43:33 -0300

    fix(sgp-ws01): fix network

--- hosts/8bbm-sgp-ws01/configuration.nix
+++ hosts/8bbm-sgp-ws01/configuration.nix
@@ -19,31 +19,6 @@
   # Hostname
   networking.hostName = "8bbm-sgp-ws01";
 
-  # Static IP configuration
-  networking.interfaces.enp0s31f6 = {
-    useDHCP = false;
-    ipv4.addresses = [
-      {
-        address = "10.0.99.22";
-        prefixLength = 24;
-      }
-    ];
-    ipv4.routes = [
-      {
-        address = "0.0.0.0";
-        prefixLength = 0;
-        via = "10.0.99.1";
-      }
-    ];
-  };
-
-  networking.nameservers = [
-    "10.242.254.70"
-    "10.242.254.71"
-    "1.1.1.1"
-    "8.8.8.8"
-  ];
-
   # Locale configuration
   myLocale = {
     defaultLocale = "en_US.UTF-8";
