93bbce443526e7012717ae0a2fd975e3e71fb78c
= <bruno.resende@gmx.com>
2026-06-06 12:30:05 -0300

    fix(bookmarks): update domain from .local to .home
    Change arr service domains to match new internal DNS scheme.

--- modules/home-manager/scripts/bookmarks.sh
+++ modules/home-manager/scripts/bookmarks.sh
@@ -10,11 +10,11 @@ Github | https://github.com/bresends?tab=repositories
 Rai - Escala | https://escala.ssp.go.gov.br
 Bastter System | https://bastter.com/bs2
 FMHY Audio Ripping | https://fmhy.net/audio#audio-ripping
-Sonarr | http://sonarr.local.cbmgo.org
-Radarr | http://radarr.local.cbmgo.org
-Lidarr | http://lidarr.local.cbmgo.org
-Bazarr | http://bazarr.local.cbmgo.org
-qBittorrent | http://qbittorrent.local.cbmgo.org
+Sonarr | http://sonarr.home.cbmgo.org
+Radarr | http://radarr.home.cbmgo.org
+Lidarr | http://lidarr.home.cbmgo.org
+Bazarr | http://bazarr.home.cbmgo.org
+qBittorrent | http://qbittorrent.home.cbmgo.org
 CBMGO Contatos e Endereços | https://www.bombeiros.go.gov.br/acesso-a-informacao/contatos-enderecos-horarios-cargos-e-ocupantes
 CBMGO Normas Administrativas | https://www.bombeiros.go.gov.br/legislacao/normas-administrativas-2.html
 CBMGO Normas Operacionais | https://www.bombeiros.go.gov.br/legislacao/normas-operacionais-administrativas/normas-operacionais-2.html
